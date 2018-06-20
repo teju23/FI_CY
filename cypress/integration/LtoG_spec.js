42073515cfebf43ed8e8c4a2d0b6d6a6e45be53e
Tejaswini <tejaswini@codeastra.com>
2018-06-20 15:54:15 +0530

    Lead to graduate journeny

--- cypress/integration/LtoG_spec.js
+++ cypress/integration/LtoG_spec.js
@@ -1,6 +1,8 @@
 describe("journey from lead to graduate",()=>{
-  it("allows user to register",()=>{
+  beforeEach(()=>{
     cy.visit('/');
+  })
+  it("allows user to register",()=>{
     let password = "secret123";
     cy.get('#location-search').type('Silicon Valley').should('have.value', 'Silicon Valley');
     cy.get('.ui-autocomplete .ui-corner-all', { timeout: 10000 });
@@ -20,43 +22,65 @@ describe("journey from lead to graduate",()=>{
         //When the semester is available at the location
       }
     })
-    cy.window().then(win => win.$('#global-apply').prop('disabled', false));
+    // cy.window().then(win => win.$('#global-apply').prop('disabled', false));
     cy.get(".registration-form").submit();
     cy.wait(1000);
-    cy.get('#education').select('have.value','3');
-    cy.get('#professional_experience').select('have.value','2');
-    cy.get('#start_up_experience').select('have.value','1');
-    cy.get('#idea_field_experience').select('have.value','1');
-    cy.get('#working_hrs_on_idea').select('have.value', '1');
-    cy.get('#team_size').select('have.value', '4');
-    cy.get('#primary_skill').select('have.value', '1');
+    cy.get('#education').select('3');
+    cy.get('#professional_experience').select('2');
+    cy.get('#start_up_experience').select('1');
+    cy.get('#idea_field_experience').select('1');
+    cy.get('#working_hrs_on_idea').select('1');
+    cy.get('#team_size').select( '4');
+    cy.get('#primary_skill').select('1');
     cy.get('#field_2').type('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.');
-    cy.get('#gender').select('have.value','4');
+    cy.get('#gender').select('4');
     cy.get('#phone_number').type('9999999999');
     cy.get('#linkedin_website').type('expert@linked.co');
-    cy.fixture('/home/codeastra/Desktop/image.jpg').as('logo')
-    cy.get('input[type=file]').then(($input)=>{
-      return Cypress.Blob.base64StringToBlob(this.logo, "image/png").then((blob) => {
-        $input.fileupload("add", { files: blob })
-      })
-    });
     cy.get('input[name="company[name]"]').type('justAName');
-    cy.get('.select').select('have.value','Meetup');
+    cy.get('.select').last().select('Meetup');
     cy.get('input[name="user[source_1]"]').type('Lorem Ipsum is simply dummy text of the printing and typesetting industry.');
     cy.get('input[value="SUBMIT APPLICATION"]').click();
     cy.wait(1000);
-    cy.contains('Successfully updated.');
+    cy.contains('Congratulations! See the next steps below.');
+    // cy.contains('Successfully updated.');
     cy.contains("Logout").click();
   }),
-  it('allows user ',()=>{
-    cy.visit('/');
-    cy.signup('sick@teju.co','teju');
-    cy.visit('http://localhost:3000/join');
-    cy.fixture('image/logo.jpg').as('logo')
-    cy.get('input[type=file]').then(($input) => {
-      return Cypress.Blob.base64StringToBlob(this.logo, "image/jpg").then((blob) => {
-        $input.fileupload("add", { files: blob })
-      })
-    });
+  it('allows admin to accept the applied user',()=>{
+    cy.signup('admin@fi.co', 'secret123');
+    cy.contains('Enroll').should('have.attr','href','/admin/enrollment?sort%5Bstatus%5D=Accepted&type=Accepted').click();
+    cy.wait(1000);
+    cy.get('.admin_select').first().select('Applied');
+    cy.wait(1000);
+    cy.get('.information-check-tool').siblings('a').last().invoke('text').should('include','Teju');
+    cy.get('.information-check-tool').get('td [id="new_status"]').last().select('Accepted');
+    cy.wait(1000);
+    cy.get('.information-check-tool').get('td [id="new_status"]').last().should('have.value','Accepted');
+    cy.get('table > tbody > tr').last().children('td').eq(6).find('input[type="submit"]').click();
   })
-})
+  it('allows to know the applied state changed to accepted',()=>{
+    cy.signup('codeastratest@gmail.com','secret123');
+    cy.url().should('eq','http://localhost:3000/join');
+    cy.contains('Step 1: Pay the Course Fee');//only for accepted founders the course fee will be asked
+    cy.contains("Logout").click();
+  }),
+  it('allows admin to make the accpeted founder to graduated',()=>{
+    cy.signup('admin@fi.co','secret123');
+    cy.contains('Enroll').should('have.attr', 'href', '/admin/enrollment?sort%5Bstatus%5D=Accepted&type=Accepted').click();
+    cy.wait(1000);
+    cy.get('.admin_select').first().select('Accepted');
+    cy.wait(1000);
+    cy.get('.information-check-tool').siblings('a').last().invoke('text').should('include','Teju');
+    cy.get('.information-check-tool').get('td [id="new_status"]').last().select('Graduated');
+    cy.wait(1000);
+   cy.get('table > tbody > tr').last().children('td').eq(6).find('input[type="submit"]').click();
+    cy.contains("Logout").click();
+  }),
+  it('allows to know the accepted founder is graduated',()=>{
+    cy.signup('codeastratest@gmail.com', 'secret123');
+    cy.get('body').then(($body) => {
+      ($body.text().includes('Graduates, like you, are the backbone of the Founder Institute.'))
+    });
+    cy.contains('Go to New Site').click();
+    cy.contains("Logout").click();
+  })
+})  
